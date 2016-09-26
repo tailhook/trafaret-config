ee23113b1417042ac6fe93e0a30573b68eed6b38
Paul Colomiets <paul@colomiets.name>
2016-09-26 12:52:21 +0300

    Add licensing info

--- setup.py
+++ setup.py
@@ -15,7 +15,13 @@ setup(name='trafaret-config',
       ],
       classifiers=[
           'Development Status :: 4 - Beta',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.3',
           'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'License :: OSI Approved :: MIT License',
+          'License :: OSI Approved :: Apache Software License',
       ],
-      )
+)
